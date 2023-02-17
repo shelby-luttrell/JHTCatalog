8814916c8e58d2b95ba801d23891ba1b05830bbe
Shelby Luttrell <shelbyal97@gmail.com>
2023-02-16 22:21:13 -0600

    cleanup unused imports and useless comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 import * as _ from 'lodash';
-// import { Observable} from "rx
-import { SearchPipe } from './search.pipe';
 import { IProduct } from './product';
 import { ProductService } from './ProductService';
 
@@ -11,21 +8,12 @@ import { ProductService } from './ProductService';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'JHTCatalog';
-  // public json: any = []
   products!: IProduct[];
   searchText!: '';
-  // product: IProduct = new IProduct();
   constructor(private productService: ProductService) { }
-
-  // getJson() {
-  //   const url = 'https://63e27e3bad0093bf29d15131.mockapi.io/api/product'
-  //   this.http.get(url).subscribe((response) => {
-  //     this.json = response
-  //     console.log(this.json)
-  //   })
-  // }
 
   ngOnInit() {
     try {
